6b048365b675acfff3c6f31c7e89cfde9f67bc96
Krishnakunwarpandey <140045769+Krishnakunwarpandey@users.noreply.github.com>
2023-07-21 20:09:55 +0530

    Add files via upload

---
+++ TypeCast.java
@@ -0,0 +1,29 @@
+package Basics;
+
+public class TypeCast {
+	public static void main(String[] args) {
+		System.out.println("main start");
+		// byte to short 
+		byte b=10;
+		short s =b;
+		System.out.println(s);
+		System.out.println("=====");
+		// int to long
+		int i = 25;
+		long l =i;
+		System.out.println(l);
+		System.out.println("=====");
+		// int to double
+		int i1 =30;
+		double d=i1;
+		System.out.println(d);
+		System.out.println("=====");
+		// char to int
+		char c ='A';
+		int a =c;
+		System.out.println(a);
+		System.out.println("main end");
+		
+	}
+
+}
